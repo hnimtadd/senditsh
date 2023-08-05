9c8576201a9061c7e3eea902af1996bf6d1654de
hnimtadd <minhdat15012002@gmail.com>
2023-08-05 09:55:43 +0700

    base: add some dependency file

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/hnimtadd/senditsh/api"
+	"github.com/hnimtadd/senditsh/config"
+	server "github.com/hnimtadd/senditsh/server"
+)
 
 func main() {
-	log.Println("Entry-point of sendi")
+	sshConfig, err := config.GetSSHConfig(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+	api, err := api.NewAPIHandlerImpl()
+	if err != nil {
+		log.Fatal(err)
+	}
+	go func() {
+		sshServer, err := server.NewSSHServerImpl(api, sshConfig)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := sshServer.Listen(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	httpConfig, err := config.GetHTTPConfig(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+	httpServer, err := server.NewHTTPServerImpl(api, httpConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := httpServer.Listen(); err != nil {
+		log.Fatal(err)
+	}
 }
