b21091424f3c22c02ea7b5cadd244380f8afd71a
hnimtadd <minhdat15012002@gmail.com>
2023-08-11 09:58:01 +0700

    sshServer: refactor code

--- server/sshServer.go
+++ server/sshServer.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"strings"
 	"time"
@@ -11,6 +12,8 @@ import (
 	"github.com/hnimtadd/senditsh/api"
 	"github.com/hnimtadd/senditsh/config"
 	"github.com/hnimtadd/senditsh/data"
+	"github.com/hnimtadd/senditsh/settings"
+	"github.com/hnimtadd/senditsh/utils"
 )
 
 type SSHServer interface {
@@ -46,62 +49,96 @@ func (server *SSHServerImpl) Listen() error {
 }
 
 func (server *SSHServerImpl) initHandler() {
-	ssh.Handle(func(s ssh.Session) {
-		ctx, cancel := context.WithTimeout(context.Background(), time.Minute*5)
+	ssh.Handle(server.TransferFileSessionHandler())
+}
+
+func (server *SSHServerImpl) TransferFileSessionHandler() ssh.Handler {
+	return func(s ssh.Session) {
+		session, err := server.initSSHSession(s)
+		if session.User != nil {
+			if err := generateVerifiedUserMessage(session, s); err != nil {
+				logger.Error("err", err)
+				s.Exit(1)
+				return
+			}
+
+		}
+		if err != nil {
+			io.WriteString(s, err.Error())
+			return
+		}
+
+		timeout := settings.Timeout
+		if session.Opt.Expired != 0 {
+			timeout = session.Opt.Expired
+		}
+
+		var (
+			ctx, cancel = context.WithTimeout(context.Background(), timeout)
+			id          = "sample"
+		)
 		defer cancel()
-		logger.Info("msg", fmt.Sprintf("New connection to server: %v, remote ip: %v, command: %v", s.Context().SessionID()[:5], s.RemoteAddr(), s.Command()))
-		opt, err := parseUserOptions(s.Command())
-		ctx = context.WithValue(ctx, "opt", opt)
-		logger.Info("msg", "parsedUserOption", "opt", opt)
+
+		session.SetContext(ctx)
+		session.Link = id
+
+		tunnel, err := server.api.InitTunnelWithID(ctx, id)
 		if err != nil {
-			logger.Error(err)
+			logger.Error("err", err)
+			io.WriteString(s, err.Error())
+			s.Exit(1)
+			return
+		}
+		defer server.api.DestroyTunnel(id)
+
+		if err := generateSetupDoneMessage(session, s); err != nil {
+			logger.Error("err", err)
 			s.Exit(1)
 			return
 		}
 
-		// id := s.Context().SessionID()
-		id := "sample"
-		ctx = context.WithValue(ctx, "link", id)
-		if err := server.api.InitTunnel(id); err != nil {
-			log.Println(err)
-			s.Exit(1)
-			return
-		}
-		logger.Info("msg", fmt.Sprintf("http://%v:%v/api/v1/transfer/%v", server.config.Host, 3000, id))
-
-		// Wait for user click shared link, if not, drop this session
-		tunnel, err := server.api.WaitToGetTunnel(ctx, id)
-		if err != nil {
-			logger.Error("msg", err)
+		if err := server.api.WaitForWriterPipeShake(ctx, id); err != nil {
+			if err := generateExpiredTransferMessage(session, s); err != nil {
+				logger.Error("error", err)
+			}
 			s.Exit(1)
 			return
 		}
 
 		// Get file from ssh connection
-		if opt.fileName != "" {
-			ctx = context.WithValue(ctx, "fileName", opt.fileName)
-		}
-		file, err := server.api.GetFileInfo(ctx, s)
+
+		file, err := server.api.GetFileInfo(*session, ctx, s)
 		if err != nil {
 			logger.Error("msg", err)
 			s.Exit(1)
 			return
 		}
-		// logger.Info("file", file)
+		session.File = file
+
 		ctx = context.WithValue(ctx, "file", file)
 
-		if err := server.api.CopyToTunnel(tunnel, file.Reader); err != nil {
-			logger.Error("msg", err)
+		if err := tunnel.PipeReader(file.Reader); err != nil {
+			logger.Error("err", err)
 			s.Exit(1)
 			return
 		}
 
-		transfer, err := server.createTransfer(ctx, s)
+		if err := tunnel.CopyInTunnel(); err != nil {
+			s.Exit(1)
+			return
+		}
+
+		transfer, err := server.createTransfer(*session)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		defer server.api.FinalizeAndCleanUpAfterTransfer(transfer, tunnel)
+		defer func() {
+			if err := generateTransferDoneMessage(session, s); err != nil {
+				logger.Error("error", err)
+			}
+			server.api.FinalizeAndCleanUpAfterTransfer(transfer)
+		}()
 
 		if err := server.api.CreateTransfer(transfer); err != nil {
 			logger.Error("msg", err)
@@ -109,141 +146,93 @@ func (server *SSHServerImpl) initHandler() {
 			return
 		}
 		return
-	})
+	}
 }
 
-// type PeekSession struct {
-// 	err error
-// 	r   io.Reader
-// }
-
-// func peekSession(s ssh.Session) chan PeekSession {
-// 	var (
-// 		peekch = make(chan PeekSession)
-// 		pr     = bufio.NewReader(s)
-// 	)
-// 	go func(r *bufio.Reader) {
-// 		b, err := r.Peek(1)
-// 		if err != nil {
-// 			peekch <- PeekSession{err: err}
-// 			return
-// 		}
-// 		if len(b) == 0 {
-// 			peekch <- PeekSession{err: fmt.Errorf("empty bytes")}
-// 			return
-// 		} else {
-// 			peekch <- PeekSession{r: pr}
-// 			return
-// 		}
-// 	}(pr)
-// 	return peekch
-// }
-
-func (server *SSHServerImpl) createTransfer(ctx context.Context, s ssh.Session) (*data.Transfer, error) {
-	file := &data.File{}
-	if fileParam := ctx.Value("file"); fileParam != nil {
-		file = fileParam.(*data.File)
+func (sever *SSHServerImpl) initSSHSession(s ssh.Session) (*api.SSHSession, error) {
+	opt, err := api.ParseUserOptions(s.Command())
+	if err != nil {
+		return nil, err
 	}
-
-	opt := &UserOptions{}
-	if optParam := ctx.Value("opt"); optParam != nil {
-		opt = optParam.(*UserOptions)
+	user := utils.GetContextVariableWithType[*data.User](s.Context(), "user", nil)
+	if user == nil {
+		logger.Info("msg", fmt.Sprintf("New connection from anonymous user: %v, remote ip: %v, command: %v", s.User(), s.RemoteAddr(), s.Command()))
+	} else {
+		logger.Info("msg", fmt.Sprintf("New connection from user: %v, remote ip: %v, command: %v", user.Username, s.RemoteAddr(), s.Command()))
 	}
-
-	from := ""
-	if fromParam := ctx.Value("from"); fromParam != nil {
-		from = fromParam.(string)
+	session, err := api.NewSSHSession(s, user, opt)
+	if err != nil {
+		return nil, err
 	}
+	return session, nil
+}
 
-	link := ""
-	if linkParam := ctx.Value("link"); linkParam != nil {
-		link = linkParam.(string)
+func (server *SSHServerImpl) createTransfer(s api.SSHSession) (*data.Transfer, error) {
+	var (
+		from = "anonymous"
+	)
+
+	if s.User != nil {
+		from = s.User.Username
 	}
 
 	transfer := data.Transfer{
-		Filename:    file.FileName,
-		From:        from,
-		Link:        link,
-		ToEmail:     opt.toEmail,
-		Message:     opt.msg,
-		Initiator:   s.User(),
-		InitiatorIP: s.RemoteAddr().String(),
-		CreatedAt:   time.Now().UnixNano(),
+		Filename:    s.File.FileName,
+		UserName:    from,
+		Link:        s.Link,
+		ToEmail:     s.Opt.ToEmail,
+		Message:     s.Opt.Msg,
+		Initiator:   s.Session.User(),
+		InitiatorIP: s.Session.RemoteAddr().String(),
+		CreatedAt:   time.Now().Unix(),
 	}
 	return &transfer, nil
 }
-
-type UserOptions struct {
-	fileName string
-	toEmail  string
-	msg      string
-}
-
-func (uOpt UserOptions) String() string {
+func generateSetupDoneMessage(session *api.SSHSession, w io.Writer) error {
 	str := strings.Builder{}
-	str.WriteString("[ ")
-	if uOpt.fileName != "" {
-		str.WriteString(fmt.Sprintf("fileName: %v ", uOpt.fileName))
-	}
-	if uOpt.msg != "" {
-		str.WriteString(fmt.Sprintf("msg: %v ", uOpt.msg))
+	str.WriteString("Direct download link:\n")
+	str.WriteString(fmt.Sprintf("\thttp://localhost:3000/api/v1/transfer/%v\n", session.Link))
+
+	user := session.User
+	if user != nil && user.Settings.Subdomain != "" {
+		str.WriteString("Download page:\n")
+		str.WriteString(fmt.Sprintf("\thttp://localhost:3000%v/%v\n", session.User.Settings.Subdomain, session.Link))
 	}
 
-	if uOpt.toEmail != "" {
-		str.WriteString(fmt.Sprintf("toEmail: %v ", uOpt.toEmail))
+	if _, err := io.WriteString(w, str.String()); err != nil {
+		return err
 	}
-	str.WriteString("]")
-	return str.String()
+	return nil
 }
 
-const (
-	fileNameDefault string = "sendit"
-	msg             string = "msg"
-	fileName        string = "filename"
-	toEmail         string = "to-email"
-	msgMinLen       int    = 2
-	msgMaxLen       int    = 50
-	fileNameMinLen  int    = 3
-	fileNameMaxLen  int    = 30
-)
+func generateExpiredTransferMessage(session *api.SSHSession, w io.Writer) error {
+	str := strings.Builder{}
+	str.WriteString("Link expired!!!")
+	str.WriteString("\n")
 
-func parseUserOptions(commands []string) (*UserOptions, error) {
-	var usrOpts = &UserOptions{}
-	for _, command := range commands {
-		parts := strings.Split(command, "=")
-		if len(parts) > 2 {
-			return nil, fmt.Errorf("option must specified in key=value format. Ex: msg=\"hell\"")
-		}
-		key := (parts[0])
-		switch key {
-		case msg:
-			if len(parts) != 2 {
-				return nil, fmt.Errorf("msg must not null\n")
-			}
-			msg := parts[1]
-			if len(msg) > msgMaxLen || len(msg) < msgMinLen {
-				return nil, fmt.Errorf("msg len must in range %v-%v characters\n", msgMinLen, msgMaxLen)
-			}
-			usrOpts.msg = msg
-
-		case fileName:
-			if len(parts) != 2 {
-				return nil, fmt.Errorf("file name must not null\n")
-			}
-			fName := parts[1]
-			if len(fName) > fileNameMaxLen || len(fName) < fileNameMinLen {
-				return nil, fmt.Errorf("file name len must in range %v-%v characters\n", msgMinLen, msgMaxLen)
-			}
-			usrOpts.fileName = fName
-
-		case toEmail:
-			if len(parts) != 2 {
-				return nil, fmt.Errorf("email must not null\n")
-			}
-			// TODO: parse valid email
-			email := parts[1]
-			usrOpts.toEmail = email
-		}
+	if _, err := io.WriteString(w, str.String()); err != nil {
+		return err
 	}
-	return usrOpts, nil
+	return nil
+}
+
+func generateTransferDoneMessage(session *api.SSHSession, w io.Writer) error {
+	str := strings.Builder{}
+	str.WriteString("Transfer done")
+	str.WriteString("\n")
+
+	if _, err := io.WriteString(w, str.String()); err != nil {
+		return err
+	}
+	return nil
+}
+func generateVerifiedUserMessage(session *api.SSHSession, w io.Writer) error {
+	str := strings.Builder{}
+	str.WriteString("Detected verified user\n")
+	str.WriteString(fmt.Sprintf("\tUserName: %s\n", session.User.Username))
+
+	if _, err := io.WriteString(w, str.String()); err != nil {
+		return err
+	}
+	return nil
 }
