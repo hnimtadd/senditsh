0cfb9cf87a26857bf4cb70448df4934e0820abfe
hnimtadd <minhdat15012002@gmail.com>
2023-08-12 18:19:45 +0700

    config: change mongo config

--- config/mongo.go
+++ config/mongo.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"fmt"
+	"log"
+	"strings"
+)
+
 type MongoConfig struct {
 	Source        string `mapstructure:"db_source"`
 	Username      string `mapstructure:"db_username"`
@@ -7,7 +13,6 @@ type MongoConfig struct {
 	Database      string `mapstructure:"db_database"`
 	AuthSource    string `mapstructure:"db_authsource"`
 	AuthMechanism string `mapstructure:"db_authmechanism"`
-	LogLevel      string `mapstructure:"db_loglevel"`
 }
 
 func GetMongoConfig(path string) (*MongoConfig, error) {
@@ -15,6 +20,17 @@ func GetMongoConfig(path string) (*MongoConfig, error) {
 	if err := LoadConfig(path, config); err != nil {
 		return nil, err
 	}
+	log.Printf("mongo config: %v\n", config)
 	return config, nil
+}
 
+func (c MongoConfig) String() string {
+	str := strings.Builder{}
+	str.WriteString(fmt.Sprintf("Database: %v\n", c.Database))
+	str.WriteString(fmt.Sprintf("UserName: %v\n", c.Username))
+	str.WriteString(fmt.Sprintf("Password: %v\n", c.Password))
+	str.WriteString(fmt.Sprintf("Source: %v\n", c.Source))
+	str.WriteString(fmt.Sprintf("AuthSource: %v\n", c.AuthSource))
+	str.WriteString(fmt.Sprintf("AuthMechanism: %v\n", c.AuthMechanism))
+	return str.String()
 }
